5ae2d3279161e2e6fab2ea9b330f0ef165a606c6
makc64 <makc120264@gmail.com>
2019-08-28 10:00:59 +0300

    init

---
+++ Calculator/Api/Model/Calculator.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Calculator\Api\Model;
+
+use Exception;
+
+class Calculator
+{
+    const DEFAULT_PRECISION = 2;
+
+    /* @var $precision */
+    private $precision;
+
+    /* @var $_left */
+    private $_left;
+
+    /* @var $right */
+    private $_right;
+
+    /* @var $_operator */
+    private $_operator;
+
+    /* @var $_allowedOperators */
+    private $_allowedOperators = [
+        'add',
+        'subtract',
+        'multiply',
+        'divide',
+        'power'
+    ];
+
+    /**
+     * Returns total result
+     *
+     * @param float|int $left
+     * @param float|int $right
+     * @param string $operator
+     * @param int|null $precision
+     * @return array result
+     * @throws Exception
+     * @api
+     */
+    public function calculate($left, $right, $operator, $precision = null)
+    {
+        $result = $status = '';
+        $this->_left = $left;
+        $this->_right = $right;
+        if (!is_null($precision)) {
+            $this->precision = $precision;
+        } else {
+            $this->precision = self::DEFAULT_PRECISION;
+        }
+
+        if (in_array($operator, $this->_allowedOperators)) {
+            $this->_operator = $operator;
+        } else {
+            $result = 'Disallowed math method';
+            $status = 'Error';
+        }
+        if (empty($result) && empty($status)) {
+            try {
+                $result = $this->calculateData();
+                $status = 'OK';
+            } catch (Exception $e) {
+                $result = $e->getMessage();
+                $status = 'Error';
+            }
+        }
+
+        return [
+            [
+                'status' => $status,
+                'result' => $result
+            ]
+        ];
+    }
+
+    /**
+     * Return finally result
+     *
+     * @return float|int|
+     */
+    private function calculateData()
+    {
+        $operationResult = '';
+        switch ($this->_operator) {
+            case 'add':
+                $operationResult = $this->_left + $this->_right;
+                break;
+            case 'subtract':
+                $operationResult = $this->_left - $this->_right;
+                break;
+            case 'multiply':
+                $operationResult = $this->_left * $this->_right;
+                break;
+            case 'divide':
+                $operationResult = $this->_left / $this->_right;
+                break;
+            case 'power':
+                $operationResult = pow($this->_left, $this->_right);
+                break;
+        }
+
+        return round($operationResult, $this->precision);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Magento2SimpleCalculator
-Magento2 simple calculator with use REST API
+# Magento2 simple calculator with use REST API
+
+Installation:
+Copy and paste(or clone) module to folder [root_magento_dir]/app/code/Tests 
+then in CLI run php bin/magento setup:upgrade
+
+How to use
+Call url http://[host-name]/rest/V1/api/rce/calculator/
+
+Parameters
+"left": First number (int|float), 
+"right": Second number (int|float), 
+"operator": Operator for operation (string), may take values - [add,subtract,multiply,divide,power]
+"precision": Precision (int) - Optional parameter , default value - 2
+
+Example request
+{
+"left": 12.34,
+"right": 56.78,
+"operator": "string",
+"precision": 2
+}
+
+Example response
+{
+"status": "OK",
+"result": 123.45
+}
